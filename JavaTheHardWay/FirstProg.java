5c94c443b316e5a960279e2599571a8a001e7c57
Zak Saylors <zsaylors11@gmail.com>
2019-08-22 22:35:56 -0400

    Finished Java The Hard Way exercise 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String[] args) {
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+    System.out.println( "I, Zak Saylors, am determined to learn how to code." );
+    System.out.println( "Today's date is Thursday, August 22, 2019." );
   }
 }
