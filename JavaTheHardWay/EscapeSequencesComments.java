52ceec6421b2e88608f3591d65159546cb21c7da
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 19:53:03 -0400

    Finished EscapeSequenceComments exercise.

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,19 @@
+public class EscapeSequencesComments {
+  public static void main ( String[] args ) {
+    // This exercise demonstrates escape sequences & comments (like these)!
+    System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n" );
+    System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
+    // System.out.frimp( "LEarn Java the Hard Way" );
+    System.out.print( "Hello\n" ); //This line prints Hello.
+    System.out.print( "Jello\by\n" ); //This line prints Jelly.
+    /* The quick brown fox jumped over a lazy dog.
+       Quick wafting zephyrs vex bold Jim. */
+    System.out /* testing */ .println( "Hard to believe, eh?" );
+    System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+    System.out.println( "\\ // -=- \\ //" );
+    System.out.println( "\\\\ \\\\\\ \\\\\\\\"); //it takes 2 to make 1
+    System.out.print( "I hope you undertand \"escape sequences\" now.\n");
+    // and comments. :)
+  }
+}
+// You cannot add a block comment between println.
