076b63545e5a575f61b965002d2931c2bbdb3001
Zak Saylors <zsaylors11@gmail.com>
2019-08-22 23:05:22 -0400

    Finished gasoline receipt exercise

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      Corner Store      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  2019-08-22  11:01PM   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  Gallons:       15.500 |" );
+    System.out.println( "|  Price/gallon: $ 2.550 |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  Fuel Total:  $ 39.53  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
