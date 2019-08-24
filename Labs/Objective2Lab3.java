83584f00c7c3edbb82aec45849e3c5139c266746
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 21:08:42 -0400

    Finished Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Ribeye";
+    food2 = "Burger";
+    food3 = "Ice Cream";
+
+    System.out.println("My Favorite Foods Are:"); //+ food1 + "\n" + food2 + "\n" + food3);
+    System.out.println(food1);
+    System.out.println(food2);
+    System.out.println(food3);
+  }
+}
