72cc9772d659275766cc012a3a0995cd983d716c
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 20:00:38 -0400

    finished study drill for exercise four.

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,5 @@
+public class PrintingChoicesEscapes {
+  public static void main( String[] args ) {
+    System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test.\n" + "I am learning " + "Java the Hard " + "Way!");
+  }
+}
