5342c95b46d25a5c984be161e97f9998287e8337
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 20:58:15 -0400

    Finished Objective2Lab2.

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Zak Saylors";
+    state = "Florida";
+    age = 27;
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
