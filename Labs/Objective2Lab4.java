f66c94db5f3bbfa9d45fc9d841ca89867891758e
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 21:12:35 -0400

    Finished Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,11 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+      num1 = 6;
+      num2 = 12;
+      num3 = 8;
+      average = (num1+num2+num3)/3;
+
+      System.out.println(average);  //Printed average
+  }
+}
