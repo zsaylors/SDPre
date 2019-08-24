4c3d617166740ffbc344517bfc624905577b95dd
Zak Saylors <zsaylors11@gmail.com>
2019-08-23 21:23:59 -0400

    Finished Objective2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,9 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10.0;
+    side2 = 8.0;
+    hypotenuse = Math.sqrt(side1*side1 + side2*side2);
+    System.out.println(hypotenuse);
+  }
+}
