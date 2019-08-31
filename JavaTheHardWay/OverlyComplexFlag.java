9df7c4e554c3a14845fb2102ef1c0cf7575129bb
Zak Saylors <zsaylors11@gmail.com>
2019-08-31 08:58:21 -0500

    Finished exercise 34.

---
+++ JavaTheHardWay/OverlyComplexFlag.java
@@ -0,0 +1,61 @@
+import static java.lang.System.*;
+
+public class OverlyComplexFlag {
+  public static void main( String[] args ) {
+        printTopHalf();
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();
+        printPledge();// study drill
+      }
+    public static void print48Colons() {
+      out.println( "|::::::::::::::::::::::::::::::::::::::::::::::::|" );
+      }
+
+    public static void print48Ohs() {
+      out.println( "|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|" );
+      }
+    public static void print29Colons() {
+      out.println( "|:::::::::::::::::::::::::::::|" );
+      }
+
+    public static void printPledge() {
+        out.println( "I pledge allegiance to the flag." );
+      }
+
+    public static void print290hs() {
+      out.println( "|OOOOOOOOOOOOOOOOOOOOOOOOOOOOO|" );
+    }
+
+    public static void print6Stars() {
+      out.print ( "| *  *  *  *  *  * " );
+    }
+
+    public static void print5Stars() {
+      out.print( "|   *  *  *  *  *  " );
+    }
+
+    public static void printSixStarLine() {
+      print6Stars();
+      print290hs();
+    }
+
+    public static void printFiveStarLine() {
+      print5Stars();
+      print29Colons();
+    }
+
+    public static void printTopHalf() {
+      out.println( " ________________________________________________" );
+      printSixStarLine();
+      printFiveStarLine();
+      printSixStarLine();
+      printFiveStarLine();
+      printSixStarLine();
+      printFiveStarLine();
+      printSixStarLine();
+    }
+  }
