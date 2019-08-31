2ce2e51dcf1dedf007de5e6bcd68525cefd7c473
Zak Saylors <zsaylors11@gmail.com>
2019-08-31 10:30:12 -0500

    Finished (or semi-finished Objective7Lab3).  Bug with closing the scanner to be fixed later.  But it's time to get back on the road

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+      printMenu();
+      selection = scanner.nextInt();
+
+      if (selection == 1) {
+        System.out.println("Hello Human");
+      }
+      else if (selection == 2) {
+        System.out.println("Apples, Bananas, Coconuts");
+      }
+      else {
+      System.out.println("Goodbye");
+      break;
+      }
+    }
+
+    //scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
