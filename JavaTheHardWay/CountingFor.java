6a0b70c202e905be52c6ee8b4824eb7704782c50
Zak Saylors <zsaylors11@gmail.com>
2019-09-05 17:18:33 -0600

    Finished exercise 44.

---
+++ JavaTheHardWay/CountingFor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountingFor {
+  public static void main( String[] args ) {
+    Scanner keyboard = new Scanner(System.in);
+    int n;
+    String message;
+
+    System.out.println( "Enter a message and I'll display it five times." );
+    System.out.print("Message: " );
+    message = keyboard.nextLine();
+
+    for (n = 1; n <= 5; n++) {
+      System.out.println(n + ". " + message );
+    }
+
+    System.out.println( "\nNow I'll show it ten times and count by 5s.");
+    for (n = 5; n <= 50; n += 5) {
+      System.out.println(n + ". " + message );
+    }
+
+    System.out.println( "\nFinally, three times counting backward." );
+    for (n = 3; n > 0; n -=1 ) { // when initialization is deleted it continues looping infinitely.
+      System.out.println(n + ". " + message );
+    }
+  }
+}
