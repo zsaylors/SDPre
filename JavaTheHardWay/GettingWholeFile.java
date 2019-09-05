93917c3a48803e360212a2a73090672464dc0a48
Zak Saylors <zsaylors11@gmail.com>
2019-09-05 16:36:58 -0600

    Finished exercise 42.

---
+++ JavaTheHardWay/GettingWholeFile.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class GettingWholeFile {
+  public static void main( String[] args ) throws Exception {
+    int fourLetter = 0;
+    int caps = 0;
+    String fn1 = "some-words.txt";
+    String fn2 = "GettingWholeFile.java";
+
+    Scanner wordReadr = new Scanner(new File(fn1));
+
+    while ( wordReadr.hasNext() ) {
+      String w = wordReadr.next();
+      if (w.length() == 4 ) {
+        fourLetter ++;
+      }
+    }
+    wordReadr.close();
+
+    Scanner selfInput = new Scanner(new File(fn2));
+    while (selfInput.hasNext() ) {
+      String token = selfInput.next();
+      if ( Character.isUpperCase(token.charAt(0))) {
+        caps++;
+      }
+    }
+    selfInput.close();
+
+    System.out.println( fourLetter + " four-letter words in " + fn1 );
+    System.out.println( caps + " words start with capitals in " + fn2 );
+  }
+}
+
+//Study Drill with four words that start with capitals.  Will they be counted? ... Yes.
