f02c64d0a012a1f08d5775b0c21ad32b880e450c
Zak Saylors <zsaylors11@gmail.com>
2019-09-05 20:34:31 -0600

    exercise 49 complete.  Need to come back to complete study drill.  != does not work with the array.

---
+++ JavaTheHardWay/ArrayLinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArrayLinearSearch {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    int[] orderNumbers = { 12345, 54321, 101010, 8675309, 31415, 271828 };
+    int toFind;
+
+    System.out.print("There are " + orderNumbers.length);
+    System.out.println(" order numbers in the database.");
+
+    System.out.print("Orders: ");
+    for (int num: orderNumbers) {
+      System.out.print(num + " ");
+    }
+    System.out.println();
+
+
+    System.out.print("Which order to find? ");
+    toFind = keyboard.nextInt();
+
+
+    //int arrayLength = orderNumbers.length;
+
+    for (int num: orderNumbers) {
+      if (num == toFind) {
+        System.out.println( num + " found.");
+      }
+    }
+  }
+}
